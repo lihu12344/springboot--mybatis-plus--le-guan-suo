0fdf8128bf5cc6dbe46b84cf52b9553c7b4bd98a
lihu <2840119419@qq.com>
2019-09-17 10:22:46 +0800

    mybatis plus 乐观锁

---
+++ src/main/java/com/example/demo/controller/StudentController.java
@@ -0,0 +1,47 @@
+package com.example.demo.controller;
+
+
+import com.example.demo.pojo.Student;
+import com.example.demo.service.StudentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * <p>
+ *  前端控制器
+ * </p>
+ *
+ * @author lihu
+ * @since 2019-09-17
+ */
+@RestController
+@RequestMapping("/student")
+public class StudentController {
+
+    @Autowired
+    private StudentService studentService;
+
+    @RequestMapping("/save")
+    public String save(){
+        Student student=new Student();
+        student.setName("瓜田李下");
+        student.setAge(23);
+
+        studentService.save(student);
+
+        return "success";
+    }
+
+    @RequestMapping("/update")
+    public Student update(){
+        Student student=studentService.getById(1);
+        student.setName("海贼王");
+
+        studentService.updateById(student);
+
+        return studentService.getById(student.getId());
+    }
+}
+
---
+++ src/main/java/com/example/demo/pojo/Student.java
@@ -0,0 +1,81 @@
+package com.example.demo.pojo;
+
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.extension.activerecord.Model;
+import com.baomidou.mybatisplus.annotation.Version;
+import com.baomidou.mybatisplus.annotation.TableId;
+import java.io.Serializable;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+
+/**
+ * <p>
+ * 
+ * </p>
+ *
+ * @author lihu
+ * @since 2019-09-17
+ */
+@ApiModel(value="Student对象", description="")
+public class Student extends Model<Student> {
+
+    private static final long serialVersionUID=1L;
+
+    @TableId(value = "id", type = IdType.AUTO)
+    private Integer id;
+
+    private String name;
+
+    private Integer age;
+
+    @Version
+    private Integer version;
+
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public Integer getVersion() {
+        return version;
+    }
+
+    public void setVersion(Integer version) {
+        this.version = version;
+    }
+
+    @Override
+    protected Serializable pkVal() {
+        return this.id;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+        "id=" + id +
+        ", name=" + name +
+        ", age=" + age +
+        ", version=" + version +
+        "}";
+    }
+}
